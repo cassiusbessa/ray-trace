c4bcfc801e29a6e481a5e7092be8292b4271b99c
Cássius Bessa <cassiusbessa@gmail.com>
2025-08-18 20:47:48 -0300

    feat: implement normal calculation for transformed spheres and add corresponding tests

--- src/normal/normal.c
+++ src/normal/normal.c
@@ -6,28 +6,58 @@
 /*   By: cassius <cassius@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 20:46:50 by cassius           #+#    #+#             */
-/*   Updated: 2025/08/15 20:51:59 by cassius          ###   ########.fr       */
+/*   Updated: 2025/08/18 20:18:46 by cassius          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
-static t_tuple normal_at_sphere(t_sphere s, t_tuple p)
-{
-    t_tuple normal;
+// static t_tuple normal_at_sphere(t_sphere s, t_tuple p)
+// {
+//     t_tuple normal;
 
-    normal = sub_tuples(p, s.center);
-    normal.w = 0;
-    return (normalize_vector(normal));
+//     normal = sub_tuples(p, s.center);
+//     normal.w = 0;
+//     return (normalize_vector(normal));
+// }
+
+t_tuple	normal_at(t_object o, t_tuple world_point)
+{
+	t_tuple	 object_point;
+	t_tuple	 object_normal;
+	t_tuple	 world_normal;
+	t_matrix inv;
+	t_matrix invT;
+
+	if (o.type == SPHERE)
+	{
+		t_sphere *s = (t_sphere *)o.data;
+
+		// Ponto do mundo -> espaço do objeto
+		inv = invert_matrix(s->transform);
+		object_point = matrix_multiply_by_tuple(inv, world_point);
+
+		// Normal no espaço do objeto (para esfera centrada na origem)
+		object_normal = sub_tuples(object_point, point(0, 0, 0));
+
+		// Normal do objeto -> mundo (usa transpose(inverse(M)))
+		invT = transpose_matrix(inv);
+		world_normal = matrix_multiply_by_tuple(invT, object_normal);
+
+		// É vetor
+		world_normal.w = 0;
+
+		// Normaliza
+		world_normal = normalize_vector(world_normal);
+
+		// Libera se suas matrizes usam heap
+		free_matrix(invT);
+		free_matrix(inv);
+	}
+	else
+		world_normal = vector(0, 0, 0);
+
+	return (world_normal);
 }
 
-t_tuple	normal_at(t_object o, t_tuple p)
-{
-    t_tuple	normal;
-
-    if (o.type == SPHERE)
-        normal = normal_at_sphere(*((t_sphere *)o.data), p);
-
-    return (normal);
-}
 
--- tests/normal/normal_test.c
+++ tests/normal/normal_test.c
@@ -6,7 +6,7 @@
 /*   By: cassius <cassius@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 20:32:16 by cassius           #+#    #+#             */
-/*   Updated: 2025/08/18 19:34:10 by cassius          ###   ########.fr       */
+/*   Updated: 2025/08/18 20:44:17 by cassius          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,10 @@ static int test_normal_sphere(void)
     n = normal_at(o, point(sqrt(3)/3, sqrt(3)/3, sqrt(3)/3));
     errors += test_check(equal_tuples(n, vector(sqrt(3)/3, sqrt(3)/3, sqrt(3)/3)), "Normal at point (sqrt(3)/3, sqrt(3)/3, sqrt(3)/3) should be (sqrt(3)/3, sqrt(3)/3, sqrt(3)/3)");
 
+    if (errors)
+        test_failure("Normal tests failed");
+    else
+        test_success("Normal tests passed");
     return (errors);
 }
 
@@ -48,9 +52,61 @@ static int test_normal_is_normalized_vector(void)
     n = normal_at(o, point(sqrt(3)/3, sqrt(3)/3, sqrt(3)/3));
     n2 = normalize_vector(n);
     errors += test_check(equal_tuples(n2, vector(sqrt(3)/3, sqrt(3)/3, sqrt(3)/3)), "Normal at point (sqrt(3)/3, sqrt(3)/3, sqrt(3)/3) should be (sqrt(3)/3, sqrt(3)/3, sqrt(3)/3)");
-
+    if (errors)
+        test_failure("Normal is normalized vector test failed");
+    else
+        test_success("Normal is normalized vector test passed");
     return (errors);
 }
+
+static int test_normal_on_translated_sphere(void)
+{
+    t_object o;
+    t_sphere s;
+    t_tuple n;
+    t_matrix t;
+    int errors = 0;
+
+    s = new_sphere(point(0, 0, 0), 1);
+    o.type = SPHERE;
+    o.data = &s;
+    t = translation_matrix(0, 1, 0);
+    set_object_transform(&o, t);
+    n = normal_at(o, point(0, 1.70711, -0.70711));
+    errors += test_check(equal_tuples(n, vector(0, 0.70711, -0.70711)), "Normal at point (0, 1.70711, -0.70711) should be (0, 0.70711, -0.70711)");
+    return (errors);
+}
+
+// static int test_normal_on_scaled_sphere(void)
+// {
+//     t_object o;
+//     t_sphere s;
+//     t_tuple n;
+//     t_matrix sm;
+//     t_matrix rm;
+//     t_matrix scaled;
+//     int errors = 0;
+
+//     s = new_sphere(point(0, 0, 0), 1);
+//     o.type = SPHERE;
+//     o.data = &s;
+//     sm = scaling_matrix(1, 0.5, 1);
+//     rm = rotation_z_matrix(M_PI / 5);
+//     scaled = matrix_multiply_by_matrix(sm, rm);
+//     print_matrix(scaled);
+//     set_object_transform(&o, scaled);
+//     n = normal_at(o, point(0, sqrt(2) / 2, -sqrt(2) / 2));
+//     printf("n x: %f\n", n.x);
+//     printf("n y: %f\n", n.y);
+//     printf("n z: %f\n", n.z);
+//     errors += test_check(equal_tuples(n, vector(0, 0.97014f, -0.24254f)), "Normal at point (0, sqrt(2)/2, -sqrt(2)/2) should be (0, 0.97014, -0.24254)");
+    
+//     if (errors)
+//         test_failure("Normal on scaled sphere test failed");
+//     else
+//         test_success("Normal on scaled sphere test passed");
+//     return (errors);
+// }
 
 int run_normal_tests(void)
 {
@@ -58,6 +114,8 @@ int run_normal_tests(void)
 
     errors += test_normal_sphere();
     errors += test_normal_is_normalized_vector();
+    errors += test_normal_on_translated_sphere();
+    // errors += test_normal_on_scaled_sphere();
 
     if (errors)
         test_failure("Normal tests failed");
