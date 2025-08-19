05dca16d17d3940ea05b9a1acc01ee46cbd925df
emorshhe <anselmiezequiel85@gmail.com>
2025-08-19 07:47:17 -0300

    Test material finish

--- tests/material/material_test.c
+++ tests/material/material_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   material_test.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cassius <cassius@student.42.fr>            +#+  +:+       +#+        */
+/*   By: emorshhe <emorshhe>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 21:57:32 by cassius           #+#    #+#             */
-/*   Updated: 2025/08/18 22:22:08 by cassius          ###   ########.fr       */
+/*   Updated: 2025/08/19 07:46:40 by emorshhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,35 @@ static int test_default_material_creation(void)
     return errors;
 }
 
+static int test_sphere_assign_material(void)
+{
+    int errors = 0;
+    t_sphere s = new_sphere(point(0, 0, 0), 1);
+    t_material m = new_material_default();
+
+    // Alterando o material
+    m.ambient = 1.0f;
+    s.material = m;
+
+    errors += test_check_double(s.material.ambient, 1.0f, "Sphere material should accept assigned ambient");
+
+    if (errors)
+        test_failure("Sphere material assignment test failed");
+    else
+        test_success("Sphere material assignment test passed");
+    return errors;
+}
+
 int run_material_tests(void)
 {
     int errors = 0;
 
     errors += test_default_material_creation();
+    errors += test_sphere_assign_material();
+
     if (errors)
-        test_failure("Material tests failed");
+        test_failure("Sphere material tests failed");
     else
-        test_success("All material tests passed");
-    return(errors);
+        test_success("All sphere material tests passed");
+    return errors;
 }
