6003a2d89ed69eadbb66cccd7af557433fc52d9c
Cássius Bessa <cassiusbessa@gmail.com>
2025-08-27 22:35:26 -0300

    feat: add test for lighting with surface in shadow

--- tests/lighting/lighting_test.c
+++ tests/lighting/lighting_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lighting_test.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emorshhe <emorshhe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cassius <cassius@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 21:57:32 by cassius           #+#    #+#             */
-/*   Updated: 2025/08/25 23:38:47 by emorshhe         ###   ########.fr       */
+/*   Updated: 2025/08/27 22:31:40 by cassius          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,12 +61,37 @@ static int test_lighting_eye_offset_45(void)
     return errors;
 }
 
+static int  test_lighting_surface_in_shadow(void)
+{
+    int errors = 0;
+
+    t_material m = new_material_default();
+    t_tuple position = point(0, 0, 0);
+    t_tuple eyev = vector(0, 0, -1);
+    t_tuple normalv = vector(0, 0, -1); // normal do ponto de teste
+    t_point_light light = new_point_light(point(0, 0, -10), new_rgb(1, 1, 1));
+
+    // Passa 1 como in_shadow
+    t_rgb result = lighting(m, light, position, eyev, normalv, 1);
+
+    errors += test_check(equal_rgb(result, new_rgb(0.1, 0.1, 0.1)),
+        "Lighting with surface in shadow should be (0.1, 0.1, 0.1)");
+
+    if (errors)
+        test_failure("Lighting test (surface in shadow) failed");
+    else
+        test_success("Lighting test (surface in shadow) passed");
+
+    return errors;
+}
+
 int run_lighting_tests(void)
 {
     int errors = 0;
 
     errors += test_lighting_eye_between_light_and_surface();
     errors += test_lighting_eye_offset_45();
+    errors += test_lighting_surface_in_shadow();
 
     if (errors)
         test_failure("Lighting tests failed");
