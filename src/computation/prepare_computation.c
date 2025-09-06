b3d26be413abcddbac737b0c70acdd1416d35b7d
emorshhe <anselmiezequiel85@gmail.com>
2025-09-06 00:25:29 -0300

    computation ok

--- src/computation/prepare_computation.c
+++ src/computation/prepare_computation.c
@@ -6,7 +6,7 @@
 /*   By: emorshhe <emorshhe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 21:54:10 by cassius           #+#    #+#             */
-/*   Updated: 2025/09/05 16:35:57 by emorshhe         ###   ########.fr       */
+/*   Updated: 2025/09/06 00:24:59 by emorshhe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 t_comps	prepare_computations(t_intersection_node i, t_ray r)
 {
-	t_comps comps;
-	float eps;
+	t_comps	comps;
+	float	eps;
 
 	comps.point = ray_position(r, i.t);
 	comps.eyev = negate_vector(r.direction);
